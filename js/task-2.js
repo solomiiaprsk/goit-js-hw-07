aded39a8085f4c878ad24b46cb53165981ca65ba
solomiiaprsk <solomiia.pruska@gmail.com>
2024-03-05 22:00:45 -0700

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,30 +1,34 @@
-class Storage {
-    #items;
+const images = [
+      {
+        url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+        alt: "White and Black Long Fur Cat",
+      },
+      {
+        url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+        alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+      },
+      {
+        url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+        alt: "Group of Horses Running",
+      },
+    ];
 
-    constructor(items) {
-        this.#items = items;
-    }
-    getItems() {
-        return this.#items;
-    }
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
-    removeItem(itemToRemove) {
-    const indexToRemove = this.#items.indexOf(itemToRemove);
+    const gallery = document.querySelector('.gallery');
 
-    if (indexToRemove !== -1) {
-      this.#items.splice(indexToRemove, 1);
-    }
-  }
-}
- 
+    const createGalleryItem = (image) => {
+      const li = document.createElement('li');
+      li.classList.add('gallery-item');
+      const img = document.createElement('img');
+      img.src = image.url;
+      img.alt = image.alt;
+      li.appendChild(img);
+      return li;
+    };
 
-  
+    const appendGalleryItems = (images) => {
+      const galleryItems = images.map(createGalleryItem);
+      gallery.append(...galleryItems);
+    };
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+    appendGalleryItems(images);
+
